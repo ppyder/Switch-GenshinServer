8dc54fe8cf9d4ad6ee5949c9eb8a7f98611616b9
ppyder <yyf942041771@outlook.com>
2022-07-12 11:08:46 +0800

    重构了工程文件夹

--- SwitchGenshinServer/Properties/AssemblyInfo.cs
+++ SwitchGenshinServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("SwitchGenshin")]
+[assembly: AssemblyTitle("SwitchGenshinServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SwitchGenshin")]
+[assembly: AssemblyProduct("SwitchGenshinServer")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("ce33dda3-d7ba-4670-925a-6876f9940918")]
+[assembly: Guid("565f1d77-e708-4b5f-8a9c-673523cac92b")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
